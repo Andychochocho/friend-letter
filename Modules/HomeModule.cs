ea075b2411ad260e179c7c450c06a0d0244045c7
Andrew Cho & Emmanuel Cutler <cho-andrew@hotmail.com & ecutler718@gmail.com>
2016-02-08 14:20:07 -0800

    add specified name through our model.recipient.

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -1,14 +1,19 @@
 using Nancy;
-
+using Letter.Objects;
 namespace FriendLetter
 {
   public class HomeModule : NancyModule
   {
     public HomeModule()
     {
-      Get["/"] = _ => View["friend-letter.html"];
+      Get["/"] = _ => {
+        LetterVariables myLetterVariables = new LetterVariables
+        {
+          Recipient = "Manny"
+        };
+        return View["friend-letter.html", myLetterVariables];
+      };
       Get["/travel-photos"] = _ => View["travel-photos.html"];
-
     }
   }
 }
