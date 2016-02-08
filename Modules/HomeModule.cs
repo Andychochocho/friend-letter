e98ced0725438e9178333eec98f8f7d38f932404
Andrew Cho & Emmanuel Cutler <cho-andrew@hotmail.com & ecutler718@gmail.com>
2016-02-08 13:56:22 -0800

    add stuff

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -1,12 +1,13 @@
 using Nancy;
 
-namespace Sample
+namespace FriendLetter
 {
   public class HomeModule : NancyModule
   {
     public HomeModule()
     {
       Get["/"] = _ => View["friend-letter.html"];
+      Get["/travel-photos"] = _ => View["travel-photos.html"];
 
     }
   }
